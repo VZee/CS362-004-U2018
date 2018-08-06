cba0eca5e66836d71d1b261371c0739a8f60d8cf
LK <16886594+VZee@users.noreply.github.com>
2018-08-06 08:59:48 -0500

    Added programmatic tests

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -20,7 +20,8 @@ public class UrlValidatorTest extends TestCase {
    // Manual Testing
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
+//You can use this function to implement your manual testing	 
+	   
 	   
    }
    ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -32,28 +33,29 @@ public class UrlValidatorTest extends TestCase {
    {
 	    // List of the string that will be used for input partitioning testing
         // All valid
-        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	    String[] scheme = {"http", "https", "ftp", "", "h3t"};
+        UrlValidator urlVal = new UrlValidator(scheme);
         assertTrue(urlVal.isValid("http://www.google.com:80/test1?action=view"));
         assertTrue(urlVal.isValid("ftp://go.au/test1/"));
         assertTrue(urlVal.isValid("10.0.0.0/$23"));
         assertTrue(urlVal.isValid("h3t://255.255.255.255/t123?action=edit&mode=up"));
    }
-
    
    public void testYourSecondPartition(){
         //You can use this function to implement your Second Partition testing	  
         // All invalid (except for query)
-        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	    String[] scheme = {"http", "https", "ftp", "", "h3t"};
+        UrlValidator urlVal = new UrlValidator(scheme);
         assertFalse(urlVal.isValid("http:256.256.256.256:-1/../"));
         assertFalse(urlVal.isValid("://:65636/.."));
         assertFalse(urlVal.isValid("3ht://.1.2.3.4:-1/../"));
         assertFalse(urlVal.isValid("http:/256.256.256.256:65a/test1//file"));
    }
-
     
    public void testYourThirdPartition(){
         // Only scheme (and query) valid
-        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	    String[] scheme = {"http", "https", "ftp", "", "h3t"};
+        UrlValidator urlVal = new UrlValidator(scheme);
         assertFalse(urlVal.isValid("h3t://1.2.3.4.:65636/test1//file?action=edit&mode=up"));
         assertFalse(urlVal.isValid(".1.2.3.4:65a/test1//file"));
         assertFalse(urlVal.isValid("ftp://:65a/..//file"));
@@ -62,7 +64,8 @@ public class UrlValidatorTest extends TestCase {
 
    public void testYourFourthPartition(){
         // Only scheme invalid
-        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	    String[] scheme = {"http", "https", "ftp", "", "h3t"};
+        UrlValidator urlVal = new UrlValidator(scheme);
         assertFalse(urlVal.isValid("http:/go.com:0/test1/?action=view"));
         assertFalse(urlVal.isValid("3ht://go.au/test1/"));
         assertFalse(urlVal.isValid("http:255.com/t123?action=edit&mode=up"));
@@ -71,7 +74,8 @@ public class UrlValidatorTest extends TestCase {
 
    public void testYourFifthPartition(){
         // Only authority (and query) valid
-        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	    String[] scheme = {"http", "https", "ftp", "", "h3t"};
+        UrlValidator urlVal = new UrlValidator(scheme);
         assertFalse(urlVal.isValid("http:go.au:65a/..//file/#"));
         assertFalse(urlVal.isValid("http/255.com:-1/..?action=view"));
         assertFalse(urlVal.isValid("://255.255.255.255:65636/..//file"));
@@ -80,7 +84,8 @@ public class UrlValidatorTest extends TestCase {
 
    public void testYourSixthPartition(){
         // Only authority invalid
-        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	    String[] scheme = {"http", "https", "ftp", "", "h3t"};
+        UrlValidator urlVal = new UrlValidator(scheme);
         assertFalse(urlVal.isValid("go.1aa/t123?action=view"));
         assertFalse(urlVal.isValid(":0/test1/?action=edit&mode=up"));
         assertFalse(urlVal.isValid("h3t://.aaa/test1/file"));
@@ -89,7 +94,8 @@ public class UrlValidatorTest extends TestCase {
 
    public void testYourSeventhPartition(){
         // Only port (and query) valid
-        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	    String[] scheme = {"http", "https", "ftp", "", "h3t"};
+        UrlValidator urlVal = new UrlValidator(scheme);
         assertFalse(urlVal.isValid("://256.256.256.256:80/../"));
         assertFalse(urlVal.isValid("3ht://go.1aa:65535/../"));
         assertFalse(urlVal.isValid("http:/.."));
@@ -98,7 +104,8 @@ public class UrlValidatorTest extends TestCase {
 
    public void testYourEigthPartition(){
         // Only port invalid
-        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	    String[] scheme = {"http", "https", "ftp", "", "h3t"};
+        UrlValidator urlVal = new UrlValidator(scheme);
         assertFalse(urlVal.isValid("http://www.google.com:-1/test1?action=view"));
         assertFalse(urlVal.isValid("ftp://0.0.0.0:65636/test1/file"));
         assertFalse(urlVal.isValid("go.com:-1?action=edit&mode=up"));
@@ -107,7 +114,8 @@ public class UrlValidatorTest extends TestCase {
 
    public void testYourNinthPartition(){
         // Only path (and query) valid
-        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	    String[] scheme = {"http", "https", "ftp", "", "h3t"};
+        UrlValidator urlVal = new UrlValidator(scheme);
         assertFalse(urlVal.isValid("://256.256.256.256:-1/$23"));
         assertFalse(urlVal.isValid("http/go.1aa/$23"));
         assertFalse(urlVal.isValid("://1.2.3.4.5/t123?action=view"));
@@ -116,7 +124,8 @@ public class UrlValidatorTest extends TestCase {
 
    public void testYourTenthPartition(){
         // Only path invalid
-        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	    String[] scheme = {"http", "https", "ftp", "", "h3t"};
+        UrlValidator urlVal = new UrlValidator(scheme);
         assertFalse(urlVal.isValid("http://www.google.com:80/#?action=view"));
         assertFalse(urlVal.isValid("h3t://:0/..//file;"));
         assertFalse(urlVal.isValid("255.255.255.255/.."));
@@ -127,12 +136,26 @@ public class UrlValidatorTest extends TestCase {
 
    ////////////////////////////////////////////////////////////////////////////////////////////////
    // Program Based Testing
-   public void testIsValid()
-   {
-	   //You can use this function for programming based testing
-
-   }
+   public void testIsValid1() {
+        // to check whether the default_schemes work
+    
+        UrlValidator urlValidator = new UrlValidator();
+    
+        assertFalse(urlValidator.isValid("http://www.example.com/devil&dog.html"));  //should be converted to ASCII
+        assertFalse(urlValidator.isValid("http://300.55.32.257.00.98.00.98"));
+        assertTrue(urlValidator.isValid("https://news.google.com"));
+        assertTrue(urlValidator.isValid("http://www.example.com/space%20here.html")); //correctly converted to ASCII
+        assertFalse(urlValidator.isValid(null));
+        assertTrue(urlValidator.isValid("http://www.amazon.com"));
+    }
+    
+    public void testIsValid2() {
+        //to test when the defined scheme is passed in
+             
+        String[] scheme = {"http", "https"};
+        UrlValidator urlValidator = new UrlValidator(scheme);
+        assertFalse(urlValidator.isValid("http:/go.com:0/test1/?action=view"));  //invalid - missing slash
+        assertTrue(urlValidator.isValid("http://www.amazon.com/?action=edit&mode=up"));         
+    }
    
-
-
 }
