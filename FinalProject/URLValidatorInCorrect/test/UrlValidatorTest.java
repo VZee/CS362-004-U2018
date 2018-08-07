4544c0262c598cecb5f3447016ee4ffd50a05f3d
LK <16886594+VZee@users.noreply.github.com>
2018-08-06 19:47:22 -0500

    Updated program based testing

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -150,7 +150,63 @@ public class UrlValidatorTest extends TestCase {
 
 
    ////////////////////////////////////////////////////////////////////////////////////////////////
-   // Program Based Testing
-   public void testIsValid1() {
+    // Program Based Testing
+    // UNIT TEST 1
+    //for loop cycles through the array of Test cases of URL schemes
+    public void testSchemeValidity() {
+        for (Test rp : testURLScheme)
+            assertEquals(
+                    rp.url,
+                    rp.expected,
+                    new UrlValidator().isValid(rp.url));
+    }
+
+    // UNIT TEST 2
+    // for loop cycles through the array of Test cases of Authority
+
+    public void testAuthorityValidity() {
+        for (Test rp : testAuthority)
+            assertEquals(
+                    rp.url,
+                    rp.expected,
+                    new UrlValidator().isValid(rp.url));
+    }
+
+    // Test Class for arrays    
+    // testURLScheme and testAuthority
+
+    static class Test {
+        String url;
+        boolean expected;
+        
+        Test(String url, boolean expected) {
+            this.url = url;
+            this.expected = expected;
+        }
+    }
+
+    Test[] testURLScheme = {
+        
+        new Test("https://news.google.com", true),
+        new Test("12345678://", false),
+        new Test("extremelylongstringthatshouldnotwork://", false),
+        new Test("(*&[*^://", false),
+        new Test("32434123://", false),
+        new Test("http:256.256.256.256:-1/../", false),
+        new Test("http:/256.256.256.256:65a/test1//file\"", false),
+        new Test("http://www.example.com/space%20here.html", true)
+    };
+
+    Test[] testAuthority = {
+        
+        new Test("192.168.1.1", true),
+        new Test("http://300.55.32.257.00.98.00.98", false),
+        new Test(":0/test1/?action=edit&mode=up", false),
+        new Test("http://192.168.1", false),
+        new Test("255.255.255.255", true),
+        new Test("http://900.55.33.257", false),
+        new Test("1.1.1.1", true),
+        new Test("http://256.256.256.256/test1?action=view", false)
+    };
 
 }
